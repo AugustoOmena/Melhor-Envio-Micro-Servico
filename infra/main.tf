ea91b0c486a42020490313ad3ca17c906f44acd4
Augusto Omena <122471298+AugustoOmena@users.noreply.github.com>
2026-02-15 11:31:42 -0300

    Update CORS configuration in API Gateway to restrict origins and expand allowed headers for enhanced security and compatibility

--- infra/main.tf
+++ infra/main.tf
@@ -108,9 +108,22 @@ resource "aws_apigatewayv2_api" "http_api" {
   protocol_type = "HTTP"
 
   cors_configuration {
-    allow_origins = ["*"]
+    allow_origins = ["https://dev.augustoomena.com"]
     allow_methods = ["GET", "POST", "OPTIONS"]
-    allow_headers = ["authorization", "content-type"]
+    allow_headers = [
+      "authorization",
+      "content-type",
+      "accept",
+      "accept-language",
+      "cache-control",
+      "origin",
+      "pragma",
+      "x-requested-with",
+      "sec-fetch-mode",
+      "sec-fetch-site",
+      "sec-fetch-dest",
+      "referer",
+    ]
   }
 }
 
