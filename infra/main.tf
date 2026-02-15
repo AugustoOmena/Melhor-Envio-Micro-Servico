27640b521b1da232a06588995816beebc4e3fc27
Augusto Omena <122471298+AugustoOmena@users.noreply.github.com>
2026-02-15 12:30:34 -0300

    Update CORS configuration in API Gateway to include allow_credentials and refine allow_origins for improved security and compatibility

--- infra/main.tf
+++ infra/main.tf
@@ -108,8 +108,9 @@ resource "aws_apigatewayv2_api" "http_api" {
   protocol_type = "HTTP"
 
   cors_configuration {
-    allow_origins = ["https://dev.augustoomena.com"]
-    allow_methods = ["GET", "POST", "OPTIONS"]
+    allow_origins     = ["https://dev.augustoomena.com"]
+    allow_credentials = false
+    allow_methods     = ["GET", "POST", "OPTIONS"]
     allow_headers = [
       "authorization",
       "content-type",
